07079d4f5889138d1626e6482f20dfd6c33b86a0
CJ3021939 <CJ3021939@LAB01-PC40>
2024-02-28 08:53:05 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_cj3021939/Program.cs
@@ -0,0 +1,12 @@
+﻿float salario;
+
+//Leitura do salario
+Console.WriteLine("Entre com seu salário:");
+salario = float.Parse( Console.ReadLine() );
+
+salario = salario + (float) (25.0f / 100.0f) * salario;
+
+
+Console.WriteLine("o novo salário é R$ {0} ", salario);
+    
+
